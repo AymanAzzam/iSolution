1e47f6b0ead098507514465814d12708ceef6f71
Ayman Azzam <aymanazzam63@gmail.com>
2024-05-03 19:15:33 +0400

    fix helm & delete install-vault script

--- App/install-vault.sh
+++
@@ -1,4 +0,0 @@
-apt-get update && apt-get install -y lsb-release 
-wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | tee /etc/apt/sources.list.d/hashicorp.list
-apt-get update && apt-get install -y vault
