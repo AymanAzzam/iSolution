66228664298f5bb3b0fe285cb8a1ad0cd9348d62
Ayman Azzam <aymanazzam63@gmail.com>
2024-05-03 17:47:34 +0400

    test pipeline

--- App/install-vault.sh
+++ App/install-vault.sh
@@ -1,4 +1,4 @@
-apt-get update && apt-get install lsb-release gpg wget -y
+apt-get update && apt-get install -y lsb-release gpg wget
 wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | tee /etc/apt/sources.list.d/hashicorp.list
-apt-get update && apt-get install vault -y
+apt-get update && apt-get install -y vault
